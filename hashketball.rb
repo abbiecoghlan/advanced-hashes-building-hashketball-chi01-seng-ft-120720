f8360280c616a513b9250b4ca853c2583d36a569
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-25 02:41:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,29 @@
-# Write your code here!
+require "pry"
+
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "string_name",
+      :colors => ["color1", "color2"],
+      :players => [
+        {:key => "value"},
+        {:key => "value"}      
+        
+        ]
+
+      
+    },
+    
+    :away => {
+       :team_name => "string_name",
+      :colors => ["color1", "color2"],
+      :players => [
+        {:key => "value"},
+        {:key => "value"}
+        
+        ]
+      
+    }  
+    
+  }
+end
