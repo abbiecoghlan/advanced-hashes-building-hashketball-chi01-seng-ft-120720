641d694e7c77349c1f3d5a716ea1b4eadf88c52d
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-25 02:52:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,25 +6,17 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        {:player_name => "value",
-        :number => "value",
-        :shoe => "value",
-        :points => "value",
-        :rebounds => "value",
-        :assists => "value",
-        :steals => "value",
-        :blocks => "value",
-        :slam_dunks => "value"},
-        
-        {:player_name => "value",
-        :number => "value",
-        :shoe => "value",
-        :points => "value",
-        :rebounds => "value",
-        :assists => "value",
-        :steals => "value",
-        :blocks => "value",
-        :slam_dunks => "value"}
+        {:player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1},
+      
+
         ]
 
       
@@ -34,8 +26,15 @@ def game_hash
        :team_name => "string_name",
       :colors => ["color1", "color2"],
       :players => [
-        {:key => "value"},
-        {:key => "value"}
+        {:player_name => "value",
+        :number => "value",
+        :shoe => "value",
+        :points => "value",
+        :rebounds => "value",
+        :assists => "value",
+        :steals => "value",
+        :blocks => "value",
+        :slam_dunks => "value"}
         
         ]
       
