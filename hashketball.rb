a838c53c27e0d3dec75b0fd79e78f9305d8e53f7
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-25 02:55:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,7 +27,7 @@ def game_hash
         :slam_dunks => 7},
         
          {:player_name => "Brook Lopez",
-        :number => 30,
+        :number => 11,
         :shoe => 14,
         :points => 12,
         :rebounds => 12,
